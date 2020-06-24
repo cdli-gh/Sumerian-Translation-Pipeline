1d33e16ab6f6080eec8e7770b4a5b8650e48102f
himanshudce <himanshu.dce12@gmail.com>
2020-06-24 12:51:29 +0530

    requirments

---
+++ requirments.sh
@@ -0,0 +1,8 @@
+pip3 install numPy
+pip3 install pandas
+pip3 install matplotlib
+pip3 install sklearn
+pip3 install tqdm
+pip3 install Keras
+pip3 install tensorflow==1.13.2
+pip3 install git+https://www.github.com/keras-team/keras-contrib.git
