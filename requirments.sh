4bdae8b9e7aa464dbc7559249d76eeb8fbd71fa6
himanshudce <himanshu.dce12@gmail.com>
2020-06-24 18:27:46 +0530

    req.

--- requirments.sh
+++ requirments.sh
@@ -2,6 +2,7 @@ pip3 install numPy
 pip3 install pandas
 pip3 install matplotlib
 pip3 install sklearn
+pip3 install sklearn_crfsuite
 pip3 install tqdm
 pip3 install Keras
 pip3 install tensorflow==1.13.2
