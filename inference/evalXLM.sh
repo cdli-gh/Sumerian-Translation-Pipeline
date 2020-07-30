2aa55bcf0ea55818511acdeb38cef18e45df0873
Rachit Bansal <rachitbansal2500@gmail.com>
2020-07-31 00:04:57 +0530

    Update evalXLM.sh

--- inference/evalXLM.sh
+++ inference/evalXLM.sh
@@ -5,9 +5,9 @@ sh inferPre.sh sum \
                $src \
                ../$model/codes \
                ../$model/vocab \
-               evaluate.sum.bpe
+               ../$model/evaluate.sum.bpe
 
-cat evaluate.sum.bpe | \
+cat ../$model/evaluate.sum.bpe | \
         python translate.py --exp_name XLMinfer123 \
         --src_lang sum --tgt_lang en \
         --model_path ../$model/checkpoint.pt --output_path $out
